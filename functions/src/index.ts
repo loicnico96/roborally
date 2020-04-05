167720be09fe1f59d59c9db779ddbc2140e5c6da
loicnico96 <loicnico96@gmail.com>
2020-04-05 18:37:00 +0200

    Setup Firebase Functions

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import firestore from './firestore'
+import functions from './functions'
+
+export const helloWorld = functions.https.onRequest(
+  async (request, response) => {
+    try {
+      await firestore.collection('items').add({ hello: 'world' })
+      response.send('Hello from Firebase!')
+    } catch {
+      response.status(500).end()
+    }
+  }
+)
