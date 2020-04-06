0636b6de79ee7d2b24108486e72009e8acaff81e
loicnico96 <loicnico96@gmail.com>
2020-04-06 09:37:47 +0200

    Setup Firestore

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, ChangeEvent, FormEvent } from 'react'
+import firebase from './firebase'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+  currentItem: string
+  username: string
 }
 
-export default App;
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      currentItem: '',
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      [e.target.name]: e.target.value
+    } as State);
+  }
+
+  handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const itemsCol = firebase.firestore().collection('items')
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsCol.add(item);
+    this.setState({
+      currentItem: '',
+      username: ''
+    });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Roborally</h1>
+            </div>
+        </header>
+        <div className='container'>
+          <section className="add-item">
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+              <button>Add Item</button>
+            </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+export default App
