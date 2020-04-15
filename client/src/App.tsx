58f88f0191b1090da3ae67116eb82d79492d55c2
loicnico96 <loicnico96@gmail.com>
2020-04-15 21:10:45 +0200

    Renamed routes.ts to utils/navigation.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { ROUTES } from './routes'
+import { ROUTES } from './utils/navigation'
 import Room from './Room'
 
 const ROOM_TEST_ID = 'id_test'
