80778d233d59edeeac21c1cf5e76d86480ea7ea6
loicnico96 <loicnico96@gmail.com>
2020-11-01 15:04:54 +0100

    Setup ESLint (functions)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: "eslint:recommended",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: [
+      "client/tsconfig.json",
+      "functions/tsconfig.json",
+    ],
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ]
 }
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
   extends: [
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
   ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
     sourceType: "module",
   },
   plugins: [
-    "@typescript-eslint",
-    "import",
     "react",
     "react-hooks",
   ],
