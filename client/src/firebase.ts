32efa35b9d41dffc3341cadbffadc418dd9b895d
loicnico96 <loicnico96@gmail.com>
2020-11-01 19:14:16 +0100

    [Functions] Add httpReady trigger in client

---
+++ client/src/firebase.ts
@@ -0,0 +1,6 @@
+import firebaseConfig from "./firebase-config"
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+firebase.initializeApp(firebaseConfig)
+export default firebase
---
+++ client/src/functions/triggers.ts
@@ -0,0 +1,24 @@
+import firebase from "firebase"
+import {
+  HttpTrigger,
+  HttpParams,
+  HttpResponse,
+  DEPLOYMENT_REGION,
+} from "common/functions"
+
+const functions = firebase.app().functions(DEPLOYMENT_REGION)
+
+export async function httpTrigger<T extends HttpTrigger>(
+  trigger: T,
+  params: HttpParams<T>
+): Promise<HttpResponse<T>> {
+  const result = await functions.httpsCallable(trigger)(params)
+  return result.data
+}
+
+export async function triggerReady(
+  params: HttpParams<HttpTrigger.READY>
+): Promise<boolean> {
+  const response = await httpTrigger(HttpTrigger.READY, params)
+  return response.success
+}
