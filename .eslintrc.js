4f87f58b514f11f798be96cd4ce8fa83e8121533
loicnico96 <loicnico96@gmail.com>
2020-11-01 15:10:58 +0100

    Setup Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
