b74b470a07fc69403ff4bf98f29eb6a58ef9e055
loicnico96 <loicnico96@gmail.com>
2020-11-02 09:46:06 +0100

    [Functions] Add useAsyncHandler

---
+++ client/src/utils/useAsyncHandler.ts
@@ -0,0 +1,30 @@
+import { useState, useCallback } from "react"
+
+export type Params = any[]
+export type Fn<P extends Params, T = any> = (...args: P) => T
+export type AsyncFn<P extends Params, T = any> = (...args: P) => Promise<T>
+export type ErrorHandler = (error: Error) => any
+
+export function useAsyncHandler<P extends Params>(
+  handler: AsyncFn<P>,
+  // eslint-disable-next-line no-console
+  onError: ErrorHandler = console.error
+): [AsyncFn<P, void>, boolean] {
+  const [running, setRunning] = useState(false)
+  const safeHandler = useCallback(
+    async (...args: P) => {
+      if (!running) {
+        try {
+          setRunning(true)
+          await handler(...args)
+        } catch (error) {
+          onError(error)
+        } finally {
+          setRunning(false)
+        }
+      }
+    },
+    [handler, running, onError]
+  )
+  return [safeHandler, running]
+}
