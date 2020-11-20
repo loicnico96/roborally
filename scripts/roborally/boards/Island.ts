ebfd75d840ac157fb754198528ab59da20ccc90e
loicnico96 <loicnico96@gmail.com>
2020-11-20 18:28:47 +0100

    [Board] Remove test lasers from Island board

--- scripts/roborally/boards/Island.ts
+++ scripts/roborally/boards/Island.ts
@@ -1,9 +1,4 @@
-import {
-  addLaser,
-  getEmptyBoard,
-  setCell,
-  setWall,
-} from "common/roborally/model/Board"
+import { getEmptyBoard, setCell, setWall } from "common/roborally/model/Board"
 import {
   getConveyor,
   getGear,
@@ -15,13 +10,9 @@ import { Direction, getPos, Rotation } from "common/roborally/model/Position"
 let board = getEmptyBoard(12, 12)
 
 board = setWall(board, getPos(0, 2), Direction.WEST)
-board = addLaser(board, getPos(0, 2), Direction.EAST, 1)
 board = setWall(board, getPos(0, 4), Direction.WEST)
-board = addLaser(board, getPos(0, 4), Direction.EAST, 1)
 board = setWall(board, getPos(0, 7), Direction.WEST)
-board = addLaser(board, getPos(0, 7), Direction.EAST, 1)
 board = setWall(board, getPos(0, 9), Direction.WEST)
-board = addLaser(board, getPos(0, 9), Direction.EAST, 1)
 board = setCell(board, getPos(0, 9), getRepair())
 
 board = setCell(board, getPos(1, 1), getHole())
@@ -30,7 +21,6 @@ board = setCell(board, getPos(1, 9), getHole())
 board = setCell(board, getPos(1, 10), getHole())
 
 board = setWall(board, getPos(2, 0), Direction.NORTH)
-board = addLaser(board, getPos(2, 0), Direction.SOUTH, 1)
 board = setCell(board, getPos(2, 1), getHole())
 board = setCell(board, getPos(2, 2), getGear(Rotation.RIGHT))
 board = setCell(board, getPos(2, 3), getConveyor(Direction.NORTH))
@@ -42,7 +32,6 @@ board = setCell(board, getPos(2, 8), getConveyor(Direction.NORTH))
 board = setCell(board, getPos(2, 9), getConveyor(Direction.WEST))
 board = setCell(board, getPos(2, 10), getHole())
 board = setWall(board, getPos(2, 11), Direction.SOUTH)
-board = addLaser(board, getPos(2, 11), Direction.NORTH, 1)
 
 board = setCell(board, getPos(3, 2), getConveyor(Direction.EAST))
 board = setCell(board, getPos(3, 3), getGear(Rotation.LEFT))
@@ -55,7 +44,6 @@ board = setCell(board, getPos(3, 8), getGear(Rotation.LEFT))
 board = setCell(board, getPos(3, 9), getConveyor(Direction.WEST))
 
 board = setWall(board, getPos(4, 0), Direction.NORTH)
-board = addLaser(board, getPos(4, 0), Direction.SOUTH, 1)
 board = setCell(board, getPos(4, 2), getConveyor(Direction.EAST))
 board = setCell(board, getPos(4, 3), getConveyor(Direction.WEST, true))
 board = setCell(board, getPos(4, 5), getConveyor(Direction.WEST))
@@ -64,7 +52,6 @@ board = setCell(board, getPos(4, 7), getHole())
 board = setCell(board, getPos(4, 8), getConveyor(Direction.EAST))
 board = setCell(board, getPos(4, 9), getConveyor(Direction.WEST))
 board = setWall(board, getPos(4, 11), Direction.SOUTH)
-board = addLaser(board, getPos(4, 11), Direction.NORTH, 1)
 
 board = setCell(board, getPos(5, 2), getConveyor(Direction.EAST))
 board = setWall(board, getPos(5, 3), Direction.NORTH)
@@ -84,7 +71,6 @@ board = setCell(board, getPos(6, 8), getConveyor(Direction.EAST))
 board = setCell(board, getPos(6, 9), getConveyor(Direction.WEST))
 
 board = setWall(board, getPos(7, 0), Direction.NORTH)
-board = addLaser(board, getPos(7, 0), Direction.SOUTH, 1)
 board = setCell(board, getPos(7, 2), getConveyor(Direction.EAST))
 board = setCell(board, getPos(7, 3), getConveyor(Direction.WEST))
 board = setCell(board, getPos(7, 4), getHole())
@@ -93,7 +79,6 @@ board = setCell(board, getPos(7, 6), getConveyor(Direction.EAST))
 board = setCell(board, getPos(7, 8), getConveyor(Direction.EAST, true))
 board = setCell(board, getPos(7, 9), getConveyor(Direction.WEST))
 board = setWall(board, getPos(7, 11), Direction.SOUTH)
-board = addLaser(board, getPos(7, 11), Direction.NORTH, 1)
 
 board = setCell(board, getPos(8, 2), getConveyor(Direction.EAST))
 board = setCell(board, getPos(8, 3), getGear(Rotation.LEFT))
@@ -105,7 +90,6 @@ board = setCell(board, getPos(8, 8), getGear(Rotation.LEFT))
 board = setCell(board, getPos(8, 9), getConveyor(Direction.WEST))
 
 board = setWall(board, getPos(9, 0), Direction.NORTH)
-board = addLaser(board, getPos(9, 0), Direction.SOUTH, 1)
 board = setCell(board, getPos(9, 1), getHole())
 board = setCell(board, getPos(9, 2), getGear(Rotation.RIGHT))
 board = setCell(board, getPos(9, 3), getConveyor(Direction.SOUTH))
@@ -118,7 +102,6 @@ board = setCell(board, getPos(9, 8), getConveyor(Direction.SOUTH))
 board = setCell(board, getPos(9, 9), getConveyor(Direction.SOUTH))
 board = setCell(board, getPos(9, 10), getHole())
 board = setWall(board, getPos(9, 11), Direction.SOUTH)
-board = addLaser(board, getPos(9, 11), Direction.NORTH, 1)
 
 board = setCell(board, getPos(10, 1), getHole())
 board = setCell(board, getPos(10, 2), getHole())
@@ -127,12 +110,8 @@ board = setCell(board, getPos(10, 10), getHole())
 
 board = setCell(board, getPos(11, 0), getRepair())
 board = setWall(board, getPos(11, 2), Direction.EAST)
-board = addLaser(board, getPos(11, 2), Direction.WEST, 1)
 board = setWall(board, getPos(11, 4), Direction.EAST)
-board = addLaser(board, getPos(11, 4), Direction.WEST, 1)
 board = setWall(board, getPos(11, 7), Direction.EAST)
-board = addLaser(board, getPos(11, 7), Direction.WEST, 1)
 board = setWall(board, getPos(11, 9), Direction.EAST)
-board = addLaser(board, getPos(11, 9), Direction.WEST, 1)
 
 export const BOARD_ISLAND = board
