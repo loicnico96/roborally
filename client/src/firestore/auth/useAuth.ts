0438da22d5df70db339f0a213b1d3210bff194ef
loicnico96 <loicnico96@gmail.com>
2020-11-22 17:21:40 +0100

    Use session auth persistence

--- client/src/firestore/auth/useAuth.ts
+++ client/src/firestore/auth/useAuth.ts
@@ -4,6 +4,12 @@ export type FirebaseAuth = firebase.auth.Auth
 export type FirebaseUser = firebase.User
 export type FirebaseUserCredential = firebase.auth.UserCredential
 
+export enum AuthPersistence {
+  LOCAL = "local",
+  SESSION = "session",
+  NONE = "none",
+}
+
 const auth = firebase.auth()
 
 export function useAuth(): FirebaseAuth {
--- client/src/firestore/auth/useSignInAnonymous.ts
+++ client/src/firestore/auth/useSignInAnonymous.ts
@@ -1,7 +1,11 @@
 import { useCallback } from "react"
-import { FirebaseUserCredential, useAuth } from "./useAuth"
+import { AuthPersistence, FirebaseUserCredential, useAuth } from "./useAuth"
 
 export function useSignInAnonymous(): () => Promise<FirebaseUserCredential> {
   const auth = useAuth()
-  return useCallback(async () => auth.signInAnonymously(), [auth])
+  return useCallback(async () => {
+    await auth.setPersistence(AuthPersistence.SESSION)
+    const userCredential = await auth.signInAnonymously()
+    return userCredential
+  }, [auth])
 }
