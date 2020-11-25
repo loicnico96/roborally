9eebfa008dac0906c3279d438749a10de0e5bb65
loicnico96 <loicnico96@gmail.com>
2020-11-25 13:29:07 +0100

    [Client] Improve style in RoomListItem

--- client/src/components/roomList/RoomListItem.tsx
+++ client/src/components/roomList/RoomListItem.tsx
@@ -1,4 +1,6 @@
-import { RoomData, RoomId } from "common/model/RoomData"
+import { GameType } from "common/GameSettings"
+import { RoomData, RoomId, RoomStatus } from "common/model/RoomData"
+import { BoardId } from "common/roborally/model/BoardData"
 import React from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
@@ -9,19 +11,45 @@ export type RoomListItemProps = {
   room: RoomData
 }
 
+function formatRoomInfo(room: RoomData): string {
+  const gameName = {
+    [GameType.ROBORALLY]: "Roborally",
+  }[room.game]
+
+  const statusName = {
+    [RoomStatus.OPENED]: "Open",
+    [RoomStatus.ONGOING]: "Ongoing",
+    [RoomStatus.FINISHED]: "Finished",
+  }[room.status]
+
+  const boardName = {
+    [BoardId.FLOOD_ZONE]: "Flood Zone",
+    [BoardId.ISLAND]: "Island",
+  }[room.options.boardId]
+
+  const playerNames = room.playerOrder.map(
+    playerId => room.players[playerId].name
+  )
+
+  return [
+    `${gameName.toUpperCase()} - ${statusName.toUpperCase()}`,
+    `Board: ${boardName}`,
+    `Players: ${playerNames.join(", ")}`,
+  ].join("\n")
+}
+
 const RoomListItemContainer = styled.div`
   background-color: #ccc;
   border: 8px solid #aaa;
   border-radius: 16px;
   margin: 24px;
   padding: 16px 24px;
+  white-space: pre-line;
 `
 
 const RoomListItem = ({ roomId, room }: RoomListItemProps) => (
   <Link to={ROUTES.room(roomId)}>
-    <RoomListItemContainer>
-      {roomId} : {JSON.stringify(room)}
-    </RoomListItemContainer>
+    <RoomListItemContainer>{formatRoomInfo(room)}</RoomListItemContainer>
   </Link>
 )
 
