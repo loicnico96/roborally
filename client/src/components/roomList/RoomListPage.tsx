37f04b71c7936de7a4d85c64c347be0fb2400951
loicnico96 <loicnico96@gmail.com>
2021-01-10 20:38:40 +0200

    [Client] Add room list loading screen

--- client/src/components/roomList/RoomListPage.tsx
+++ client/src/components/roomList/RoomListPage.tsx
@@ -5,17 +5,12 @@ import styled from "styled-components"
 import { GameType } from "common/GameSettings"
 import { RoomData } from "common/model/RoomData"
 import AsyncButton from "components/ui/AsyncButton"
-import PageContainer from "components/ui/PageContainer"
-import PageContent from "components/ui/PageContent"
-import PageHeader from "components/ui/PageHeader"
 import { useAuthContext } from "firestore/auth/AuthContext"
 import { triggerRoomCreate } from "functions/triggers"
 import { ROUTES } from "utils/navigation"
 import { LoadedResource } from "utils/resources"
 
 import RoomList from "./RoomList"
-
-const NAVIGATION_PARENTS = [{ title: "HOME", path: ROUTES.home() }]
 
 export type RoomListPageProps = {
   rooms: LoadedResource<RoomData>[]
@@ -41,17 +36,14 @@ const RoomListPage = ({ rooms }: RoomListPageProps) => {
   const [createRoom, isCreateRoomEnabled] = useCreateRoom()
 
   return (
-    <PageContainer>
-      <PageHeader parents={NAVIGATION_PARENTS} title="ROOMS" />
-      <PageContent>
-        <RoomListPageHeader>
-          <AsyncButton onClick={createRoom} disabled={!isCreateRoomEnabled}>
-            Create room
-          </AsyncButton>
-        </RoomListPageHeader>
-        <RoomList rooms={rooms} />
-      </PageContent>
-    </PageContainer>
+    <>
+      <RoomListPageHeader>
+        <AsyncButton onClick={createRoom} disabled={!isCreateRoomEnabled}>
+          Create room
+        </AsyncButton>
+      </RoomListPageHeader>
+      <RoomList rooms={rooms} />
+    </>
   )
 }
 
--- client/src/components/roomList/RoomListRoute.tsx
+++ client/src/components/roomList/RoomListRoute.tsx
@@ -3,7 +3,13 @@ import React, { useState } from "react"
 import { Collection } from "common/firestore/collections"
 import { RoomData } from "common/model/RoomData"
 import { SortDirection } from "common/utils/arrays"
+import PageContainer from "components/ui/PageContainer"
+import PageContent from "components/ui/PageContent"
+import { renderError } from "components/ui/PageError"
+import PageHeader from "components/ui/PageHeader"
+import { renderLoader } from "components/ui/PageLoader"
 import { useCollectionLoader } from "firestore/useCollectionLoader"
+import { ROUTES } from "utils/navigation"
 import {
   Resource,
   isLoading,
@@ -13,6 +19,8 @@ import {
 } from "utils/resources"
 
 import RoomListPage from "./RoomListPage"
+
+const NAVIGATION_PARENTS = [{ title: "HOME", path: ROUTES.home() }]
 
 const RoomListRoute = () => {
   const [roomsResource, setRoomsResource] = useState<
@@ -24,15 +32,20 @@ const RoomListRoute = () => {
     sortField: "createdAt",
   })
 
-  if (isLoading(roomsResource)) {
-    return <div>Loading rooms...</div>
-  }
-
-  if (isError(roomsResource)) {
-    return <div>Failed to load rooms</div>
-  }
-
-  return <RoomListPage rooms={roomsResource.data} />
+  return (
+    <PageContainer>
+      <PageHeader parents={NAVIGATION_PARENTS} title="ROOMS" />
+      <PageContent>
+        {isLoading(roomsResource) ? (
+          renderLoader("Loading rooms...")
+        ) : isError(roomsResource) ? (
+          renderError(roomsResource.error)
+        ) : (
+          <RoomListPage rooms={roomsResource.data} />
+        )}
+      </PageContent>
+    </PageContainer>
+  )
 }
 
 export default RoomListRoute
