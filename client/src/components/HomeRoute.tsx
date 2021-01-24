72bc3bd695bd2e4303fcb3bb6c54a128fa4badd0
loicnico96 <loicnico96@gmail.com>
2021-01-24 19:20:46 +0100

    [Client] Improve home page

--- client/src/components/HomeRoute.tsx
+++ client/src/components/HomeRoute.tsx
@@ -1,16 +1,46 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import styled from "styled-components"
 
+import { BoardId } from "common/roborally/model/BoardData"
 import PageContainer from "components/ui/PageContainer"
 import PageContent from "components/ui/PageContent"
 import PageHeader from "components/ui/PageHeader"
 import { ROUTES } from "utils/navigation"
 
+import { getBoardImage } from "./roborally/BoardImage"
+import Box from "./ui/Box"
+
+
+const GameContainer = styled.div`
+  width: 300px;
+`
+
+const GameIcon = styled(Box)`
+  background-image: url("${getBoardImage(BoardId.ISLAND)}");
+  background-position: -36px -36px;
+  background-size: 600px 600px;
+  box-sizing: border-box;
+  height: 300px;
+  width: 300px;
+`
+
+const GameTitle = styled.div`
+  font-size: 1.5em;
+  padding-top: 0.5em;
+  text-align: center;
+`
+
 const HomeRoute = () => (
   <PageContainer>
     <PageHeader title="HOME" />
     <PageContent>
-      <Link to={ROUTES.roomList()}>Open rooms</Link>
+      <GameContainer>
+        <Link to={ROUTES.roomList()} title="Click to open rooms">
+          <GameIcon />
+          <GameTitle>Roborally</GameTitle>
+        </Link>
+      </GameContainer>
     </PageContent>
   </PageContainer>
 )
