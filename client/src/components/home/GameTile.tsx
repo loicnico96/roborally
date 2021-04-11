74fbeeeb4d04a8226b7c5d7a0f33b3b26220e11a
loicnico96 <loicnico96@gmail.com>
2021-04-11 18:59:56 +0200

    [Client] Change home screen images

--- client/src/components/home/GameTile.tsx
+++ client/src/components/home/GameTile.tsx
@@ -2,20 +2,29 @@ import React from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 
+import HomeImageMetropolys from "assets/metropolys/home.jpg"
+import HomeImageRoborally from "assets/roborally/home.jpg"
 import { GameType } from "common/GameSettings"
-import { BoardId } from "common/roborally/model/BoardData"
-import { getBoardImage } from "components/game/roborally/BoardImage"
 import { getGameLabel } from "components/roomList/utils/getters"
 import Box from "components/ui/Box"
 import { ROUTES, withSearchParams } from "utils/navigation"
 
-const StyledIcon = styled(Box)`
-  background-image: url("${getBoardImage(BoardId.ISLAND)}");
-  background-position: -36px -36px;
-  background-size: 600px 600px;
+export type GameTileProps = {
+  gameType: GameType
+}
+
+const StyledTile = styled(Box)`
+  align-items: center;
   box-sizing: border-box;
+  display: flex;
   height: 300px;
+  justify-content: center;
   width: 300px;
+`
+
+const StyledImage = styled.img`
+  max-height: 100%;
+  max-width: 100%;
 `
 
 const StyledLabel = styled.div`
@@ -24,16 +33,19 @@ const StyledLabel = styled.div`
   text-align: center;
 `
 
-export type GameTileProps = {
-  gameType: GameType
-}
-
 const GameTile = ({ gameType }: GameTileProps) => {
   const linkTo = withSearchParams(ROUTES.roomList(), { game: gameType })
 
+  const imageUrl = {
+    [GameType.METROPOLYS]: HomeImageMetropolys,
+    [GameType.ROBORALLY]: HomeImageRoborally,
+  }[gameType]
+
   return (
     <Link to={linkTo} title="Click to show rooms">
-      <StyledIcon />
+      <StyledTile>
+        <StyledImage src={imageUrl} />
+      </StyledTile>
       <StyledLabel>{getGameLabel(gameType)}</StyledLabel>
     </Link>
   )
